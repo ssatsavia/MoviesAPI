6e3a90350100cb168df8329c78911a4ea6479313
ssatsavia <145367186+ssatsavia@users.noreply.github.com>
2024-10-16 11:45:56 -0400

    commit

--- server.js
+++ server.js
@@ -7,7 +7,8 @@
 *  Name: sage satsavia Student ID: 132238197 Date: _____09/24/2024___________
 *  Vercel Link: _______________________________________________________________
 *
-********************************************************************************/ const express = require('express');
+********************************************************************************/ 
+const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const app = express();
@@ -27,8 +28,7 @@ db.initialize(process.env.MONGODB_CONN_STRING)
   .catch((err) => {
     console.error('Error connecting to the database:', err);
   });
-
-  app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.json({ message: "API Listening" });
 });
 
